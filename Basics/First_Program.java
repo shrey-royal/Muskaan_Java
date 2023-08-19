bd3ca1491655697cc77692fd62241c68d715dd2c
Shrey K <srkkadia@gmail.com>
2023-08-19 20:02:17 +0530

    init

---
+++ Basics/First_Program.java
@@ -0,0 +1,14 @@
+public class First_Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+
+To run this program: 
+    1. Open Command Prompt
+    2. Navigate to the folder where this file is located
+    3. Type: javac First_Program.java
+    4. Type: java First_Program
+*/
