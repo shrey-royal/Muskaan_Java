8b4df0d5bb54493b0a209e42e8ad255ba5bbc2f3
Shrey K <srkkadia@gmail.com>
2024-01-27 16:01:46 +0530

    Contact_App

---
+++ Project/contacts.py
@@ -0,0 +1,6 @@
+""" This module provides the Contacts entry point script."""
+
+from contacts.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ Project/contacts/__init__.py
@@ -0,0 +1,3 @@
+"""This module provides the contacts package"""
+
+__version__ = "0.1.0"
